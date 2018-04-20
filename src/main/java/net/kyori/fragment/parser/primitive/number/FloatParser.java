97a2f4c5657566d8b22a704dc8413561f8de21ae
kashike <kashike@vq.lc>
2018-04-19 21:34:07 -0700

    bring parsers in

---
+++ src/main/java/net/kyori/fragment/parser/primitive/number/FloatParser.java
@@ -0,0 +1,44 @@
+/*
+ * This file is part of fragment, licensed under the MIT License.
+ *
+ * Copyright (c) 2018 KyoriPowered
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package net.kyori.fragment.parser.primitive.number;
+
+import net.kyori.xml.XMLException;
+import net.kyori.xml.node.Node;
+
+import javax.inject.Singleton;
+
+/**
+ * Parses a {@link Node} into a {@link Float float}.
+ */
+@Singleton
+public class FloatParser implements NumberParser<Float> {
+  @Override
+  public Float throwingParse(final Node node, final String string) throws XMLException {
+    try {
+      return Float.parseFloat(string);
+    } catch(final NumberFormatException e) {
+      throw new XMLException("Could not parse '" + string + "' as a float", e);
+    }
+  }
+}
